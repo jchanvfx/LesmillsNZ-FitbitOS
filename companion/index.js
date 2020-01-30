741d806b1d162d1ba037004a9ba777c7a7a0146f
jchanvfx <jjohnnycchan@gmail.com>
2020-01-31 11:39:24 +1300

    wip data fetch and settings map.

--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,94 @@
-console.log('companion run!');
+import * as messaging from "messaging";
+import * as lesMills from "./utils/les_mills";
+import { me as companion } from "companion";
+import { settingsStorage } from "settings";
+
+
+// Check internet permissions
+if (!companion.permissions.granted("access_internet")) {
+    console.log("We're not allowed to access the internet!");
+}
+
+// Message socket opens (send)
+messaging.peerSocket.onopen = function() {
+    console.log("Companion Socket Open");
+    // restoreSettings();
+  };
+
+// Message socket closes
+messaging.peerSocket.onclose = () => {
+    console.log("Companion Socket Closed");
+}
+
+// Message is received
+messaging.peerSocket.onmessage = function(evt) {
+    if (evt.data.key === "lm-refresh") {
+        let clubSettings = settingsStorage.getItem("clubID")
+        if (clubSettings != null) {
+            let selectedClub = JSON.parse(clubSettings).values;
+            let clubID = selectedClub.value;
+            let clubName = selectedClub.name;
+
+            // send fetch data here.............
+            console.log('--- LesMills fetch request ---');
+            // let data = lesMills.fetchLesMillsData(clubID);
+
+            let data = {
+                key: "lm-timetable",
+                value: {cid: clubID, name: clubName, data: null}
+            };
+            sendValue(data);
+        } else {
+            console.log('Club location not set.')
+            let data = {
+                key: "lm-noClub",
+                value: "Club location not set in app settings."
+            };
+            sendValue(data);
+        }
+
+    }
+}
+
+// Listen for the onerror event
+messaging.peerSocket.onerror = function(err) {
+    // Handle any errors
+    console.log("Connection error: " + err.code + " - " + err.message);
+}
+
+// Settings changed callback
+settingsStorage.onchange = function(evt) {
+    let data = {key: evt.key, newValue: evt.newValue};
+    sendValue(data);
+}
+
+// Send data to Fitbit device using Messaging API
+function sendValue(data) {
+    if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+        messaging.peerSocket.send(data);
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+
+// Restore any previously saved settings and send to the device
+function restoreSettings() {
+    for (let index = 0; index < settingsStorage.length; index++) {
+        let key = settingsStorage.key(index);
+        if (key) {
+            let data = {key: key, newValue: settingsStorage.getItem(key)};
+            sendValue(data);
+        }
+    }
+}
+
+// Fetch Timetable Data from LesMills Web API
+// let url = "https://www.lesmills.co.nz/api/timetable/get-timetable-epi"
+// fetch(url, {
+//     method: 'POST',
+//     headers: new Headers({'Content-type': 'application/json'}),
+//     body: JSON.stringify({Club: "04"})
+// })
+// .then(response => response.json())
+// .then(data => console.log(data));
--- settings/index.jsx
+++ settings/index.jsx
@@ -1,42 +1,37 @@
 function settingsComponent(props) {
-  return (
-    <Page>
-      <Section title={<Text bold align="center">App Settings</Text>}/>
-      <Select
-        title="Club Location"
-        label={`Selection`}
-        settingsKey="club"
-        options={[
-          {name:"AUCKLAND CITY",    value:"01", addr:"186 Victoria Street, Auckland"},
-          {name:"BRITOMART",        value:"09", addr:"2 Britomart Place, Britomart"},
-          {name:"NEWMARKET",        value:"13", addr:"269 Khyber Pass Road, Newmarket"},
-          {name:"NEW LYNN",         value:"07", addr:"2-4 Rankin Ave, New Lynn"},
-          {name:"TAKAPUNA",         value:"06", addr:"Cnr Lake Road &, Como St, Shore City, Takapuna"},
-          {name:"HOWICK",           value:"12", addr:"100 Whitford Rd, Somerville"},
-          {name:"HAMILTON",         value:"05", addr:"747 Victoria Street, Hamilton"},
-          {name:"HUTT CITY",        value:"08", addr:"7 Pretoria St, Lower Hutt"},
-          {name:"TARANAKI STREET",  value:"04", addr:"52-70 Taranaki St, Wellington"},
-          {name:"LAMBTON QUAY",     value:"10", addr:"86-90 Lambton Quay, Wellington"},
-          {name:"CHRISTCHURCH",     value:"02", addr:"203 Cashel St, Christchurch Central"},
-          {name:"DUNEDIN",          value:"03", addr:"12 Dowling St, Dunedin"},
-        ]}
-        renderItem={
-          (option) =>
-          <TextImageRow
-          label={option.name}
-          sublabel={option.addr}
-          />
-        }
-        onSelection={(selection) => console.log(selection)}
-      />
+    return (
+        <Page>
+            <Section title={<Text bold align="center">App Settings</Text>}/>
+            <Select
+                title="Club"
+                label={`Selection`}
+                settingsKey="clubID"
+                options={[
+                    {name:"AUCKLAND CITY",    value:"01", addr:"186 Victoria Street, Auckland"},
+                    {name:"TARANAKI STREET",  value:"04", addr:"52-70 Taranaki St, Wellington"},
+                    {name:"LAMBTON QUAY",     value:"10", addr:"86-90 Lambton Quay, Wellington"},
+                    {name:"BRITOMART",        value:"09", addr:"2 Britomart Place, Britomart"},
+                    {name:"NEWMARKET",        value:"13", addr:"269 Khyber Pass Road, Newmarket"},
+                    {name:"NEW LYNN",         value:"07", addr:"2-4 Rankin Ave, New Lynn"},
+                    {name:"TAKAPUNA",         value:"06", addr:"Cnr Lake Road &, Como St, Shore City, Takapuna"},
+                    {name:"HOWICK",           value:"12", addr:"100 Whitford Rd, Somerville"},
+                    {name:"HAMILTON",         value:"05", addr:"747 Victoria Street, Hamilton"},
+                    {name:"HUTT CITY",        value:"08", addr:"7 Pretoria St, Lower Hutt"},
+                    {name:"CHRISTCHURCH",     value:"02", addr:"203 Cashel St, Christchurch Central"},
+                    {name:"DUNEDIN",          value:"03", addr:"12 Dowling St, Dunedin"},
+                ]}
+                renderItem={
+                    (option) =>
+                    <TextImageRow
+                    label={option.name}
+                    sublabel={option.addr}
+                    />
+                }
+                // onSelection={(selection) => console.log(selection)}
+            />
 
-      <Button
-        label="Save Settings"
-        onClick={() => console.log("Clicked!")}
-      />
-
-    </Page>
-  );
+        </Page>
+    );
 }
 
 registerSettingsPage(settingsComponent);
