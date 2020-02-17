fa7ff83c53a2e6b4680c584953f03ae89cbc11d9
Johnny Chan <7546915+jchanvfx@users.noreply.github.com>
2020-02-18 09:35:54 +1300

    cleanup

--- app/datelib.js
+++ app/datelib.js
@@ -23,16 +23,3 @@ export const formatTo12hrTime = date => {
     hours = hours ? hours : 12; // hour '0' should be '12'
     return hours + ':' + mins + ampm;
 }
-// export const getDateObj = () => {return new Date();};
-// export const getDate = () => {return getDateObj().getDate();};
-// export const getYear = () => {return getDateObj().getFullYear();};
-// export const getDayName = () => {return DAYS[getDateObj().getDay()];};
-// export const getDayShortName = () => {return DAYS_SHORT[getDateObj().getDay()];};
-// export const getMonthName = () => {return MONTHS[getDateObj().getMonth()];};
-// export const getMonthShortName = () => {return MONTHS_SHORT[getDateObj().getMonth()];};
-// export const getTime12hr = () => {return formatTo12hrTime(getDateObj());};
-// export const getTime24Hr = () => {
-//     let date = getDateObj();
-//     return zeroPad(date.getHours + ':' + zeroPad(date.getMinutes()));
-// };
-// function zeroPad(i) {if (i < 10) {i = "0" + i;} return i;}
