003ac1ebf672a393e38a0180b05b4aa1b9b44819
jchanvfx <jjohnnycchan@gmail.com>
2020-03-06 08:04:17 +1300

    version bump 0.1.1

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
-export const BUILD_VER = '0.1.0';
-export const DEBUG_MODE = false;
-export const DATA_FILE_PREFIX = "LM_dat";
-export const SETTINGS_FILE = "LM_settings.cbor";
-export const CLASSES_FILE = "LM_classes.cbor";
+export const BUILD_VER          = '0.1.1';
+export const DEBUG_MODE         = false;
+export const DATA_FILE_PREFIX   = "LM_dat";
+export const SETTINGS_FILE      = "LM_settings.cbor";
+export const CLASSES_FILE       = "LM_classes.cbor";
