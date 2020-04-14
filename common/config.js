0b8f8fbb39c8acf5006232203c6ad1fe451acb3a
jchanvfx <jjohnnycchan@gmail.com>
2020-04-14 19:13:08 +1200

    clean up

--- common/config.js
+++ common/config.js
@@ -1,4 +1,4 @@
-export const BUILD_VER          = '0.2.2';
+export const BUILD_VER          = '0.2.3';
 export const DEBUG_MODE         = false;
 export const DATA_FILE_PREFIX   = "lm_workouts.LM_dat";
 export const SETTINGS_FILE      = "lm_workouts.settings.cbor";
