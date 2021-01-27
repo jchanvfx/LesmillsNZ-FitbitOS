f19220bb390fc6ebc7eb5b00d55a3668255975c9
jchanvfx <jjohnnycchan@gmail.com>
2021-01-27 22:53:30 +1300

    back to top tile.

--- common/config.js
+++ common/config.js
@@ -1,4 +1,4 @@
-export const BUILD_VER          = '0.3.12';
+export const BUILD_VER          = '0.3.13';
 export const DEBUG_MODE         = false;
 export const DATA_FILE_PREFIX   = "lm_timetable.LM_dat";
 export const SETTINGS_FILE      = "lm_timetable.settings.cbor";
